ebe16566535aef978b7cf71a954ac71ce6ef1dd9
samanvitha <samanvitharao12345@gmail.com>
2022-04-26 23:19:51 +0530

    Created class TestController

--- src/test/java/com/dlithe/bankingapp/BankingAppApplicationTests.java
+++ src/test/java/com/dlithe/bankingapp/BankingAppApplicationTests.java
@@ -1,13 +1,13 @@
 package com.dlithe.bankingapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BankingAppApplicationTests {
+import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-	@Test
-	void contextLoads() {
+@SpringBootApplication
+public class BankingAppApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppApplication.class, args);
 	}
-
 }
