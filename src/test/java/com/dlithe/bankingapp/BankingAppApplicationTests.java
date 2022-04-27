c16462e0af2c78e64409152529278afa5c5a40e2
samanvitha <samanvitharao12345@gmail.com>
2022-04-27 19:39:50 +0530

    Created class TestController, serivces,ServiceImplementation

--- src/test/java/com/dlithe/bankingapp/BankingAppApplicationTests.java
+++ src/test/java/com/dlithe/bankingapp/BankingAppApplicationTests.java
@@ -1,13 +1,15 @@
 package com.dlithe.bankingapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BankingAppApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+
+		SpringApplication.run(BankingAppApplication.class, args);
 	}
 
 }
---
+++ src/test/java/com/dlithe/bankingapp/Controller/TestController.java
@@ -0,0 +1,49 @@
+package com.dlithe.bankingapp.Controller;
+
+import com.dlithe.bankingapp.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+    @GetMapping("fetch-Product-Details/{productName}")
+    public String myFirstMethod(@PathVariable String productName) {
+        String result = testService.fetchProductDetails(productName);
+        return result;
+    }
+}
+
+//    @GetMapping("test")
+//    public String myFirstMethod() {
+//        return "Welcome to java";
+//    }
+
+//    @GetMapping("details/{productNames}")
+//    public String cosmetics(@PathVariable String productNames) {
+//        switch (productNames) {
+//            case "saress":
+//                return saressDetails();
+//            case "makeup":
+//                return makeupDetails();
+//            default:
+//                return "Not Found";
+//        }
+//
+//    }
+//
+//    public String saressDetails() {
+//        return "Saress is the best in the market with price varying from 500-10000 INR";
+//    }
+//
+//    public String makeupDetails() {
+//        return "Limited edition makeup with only 8 pieces in stock all over the world";
+//    }
+//}
+//
+
